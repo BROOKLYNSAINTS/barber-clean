6bea92dd8b6fea5f8fea3fd879fb754d37fd0758
Joseph Murphy <coach@brooklynsaints.com>
2025-09-16 15:37:40 -0400

    fix(ios): add camera/photo usage strings and bump build number

--- app.config.js
+++ app.config.js
@@ -4,10 +4,15 @@ export default {
   slug: 'barber-clean',
   version: '1.0.1',
   ios: {
-    buildNumber: '65',
+    buildNumber: '66', // bump for resubmission
     bundleIdentifier: 'com.ScheduleSync.barber',
     supportsTablet: true,
-    infoPlist: { ITSAppUsesNonExemptEncryption: false }
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+      NSCameraUsageDescription: 'We use the camera to let you take or upload profile and shop photos.',
+      NSPhotoLibraryUsageDescription: 'We access your photo library so you can choose photos to upload.',
+      NSPhotoLibraryAddUsageDescription: 'We save images to your photo library when you choose to export or save.'
+    }
   },
   android: {
     package: 'com.ScheduleSync.barber'
@@ -16,7 +21,6 @@ export default {
     eas: { projectId: '34c586b7-af2c-411d-9fbd-5cb699e2b12e' }
   },
   plugins: [
-    'expo-asset',
-    'expo-calendar'
+    'expo-asset'
   ]
 };
