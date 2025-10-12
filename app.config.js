f0aa9081a696b52e0e64a93558897d73fbf24aa4
Joseph Murphy <coach@brooklynsaints.com>
2025-10-11 21:47:29 -0400

    Bump build number for clean TestFlight build

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ module.exports = ({ config }) => ({
   ios: {
     bundleIdentifier: 'com.ScheduleSync.barber',
     supportsTablet: true,
-    buildNumber: '135', // bump for TestFlight
+    buildNumber: '137', // bump for new build
     jsEngine: 'hermes',
     runtimeVersion: { policy: 'appVersion' },
     infoPlist: {
@@ -42,20 +42,20 @@ module.exports = ({ config }) => ({
   android: {
     package: 'com.ScheduleSync.barber',
     adaptiveIcon: { foregroundImage: './assets/adaptive-icon.png', backgroundColor: '#FFFFFF' },
-    versionCode: 2,
+    versionCode: 3,
     jsEngine: 'hermes',
     runtimeVersion: { policy: 'appVersion' },
   },
   extra: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    FIREBASE_API_KEY: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    STRIPE_PUBLISHABLE_KEY: process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    BACKEND_URL: process.env.EXPO_PUBLIC_BACKEND_URL,
     eas: { projectId: '34c586b7-af2c-411d-9fbd-5cb699e2b12e' },
   },
   plugins: [
